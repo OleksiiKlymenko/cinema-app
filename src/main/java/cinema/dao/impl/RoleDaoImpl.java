e100f51030dcb512ea94cedb2b76f9cb79167eca
Oleksii Klymenko <alexeyklimenkowork@gmail.com>
2023-05-22 11:38:00 +0300

    project commit

---
+++ src/main/java/cinema/dao/impl/RoleDaoImpl.java
@@ -0,0 +1,51 @@
+package cinema.dao.impl;
+
+import cinema.dao.AbstractDao;
+import cinema.dao.RoleDao;
+import cinema.exception.DataProcessingException;
+import cinema.model.Role;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RoleDaoImpl extends AbstractDao<Role> implements RoleDao {
+    public RoleDaoImpl(SessionFactory factory) {
+        super(factory, Role.class);
+    }
+
+    @Override
+    public Role add(Role role) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.save(role);
+            transaction.commit();
+            return role;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can`t add role " + role, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public Role getByName(Role.RoleName roleName) {
+        Transaction transaction = null;
+        try (Session session = factory.openSession()) {
+            return session.createQuery("FROM Role WHERE roleName = :roleName", Role.class)
+                    .setParameter("roleName", roleName)
+                    .getSingleResult();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can`t get role by name: " + roleName, e);
+        }
+    }
+}
